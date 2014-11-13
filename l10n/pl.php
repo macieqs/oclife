7158fde5b84ddd876505378cf3d8d3a48dd6b3ce
root <root@trumpet.nd.s4.stermedia.eu>
2014-11-13 14:22:40 +0100

    Create polish translations

---
+++ l10n/pl.php
@@ -0,0 +1,89 @@
+<?php
+$TRANSLATIONS = array(
+// Admin setup
+// - php and templates
+'ImageMagick is loaded and ready to be used' => 'ImageMagick jest załadowany i gotowy do użycia.',
+'ImageMagick is not loaded: Please refers to php manual.' => 'Błąd ładowania ImageMagick. Zasięgnij porad z instrukcji użytkowania.',
+'Only admins can edit tags' => 'Tylko administratorzy mogą edytować tagi.',
+'Use ImageMagick instead of GD (More image formats handled)' => 'Używaj ImageMagick zamiast GD (obsługuje więcej formatów)',
+
+// - JavaScript
+'Settings not saved! Data base error!', 'Ustawienia nie zostały zapisane! Błąd bazy danych',
+'Settings not saved! Communication error!', 'Ustawienia nie zostały zapisane! Błąd komunikacji!',
+
+// Main app
+// - php and templates
+'Actual tags' => 'Obecne Tagi',
+'Expand all' => 'Rozwiń wszystko',
+'Collapse all' => 'Zwiń wszystko',
+'Rename tag' => 'Zmień nazwę',
+'Rename the tag and confirm.' => 'Zmień nazwę i potwierdź',
+'Create a new tag' => 'Utwórz nowy tag',
+'Insert the new tag and confirm.' => 'Wprowadź nowy tag i potwierdź',
+'Delete tag' => 'Usuń tag',
+'Really delete the tag:' => 'Potwiedź usuwanie:',
+'NOTE: ' => 'UWAGA: ',
+'Also child tags will be removed!' => 'Wszystkie tagi podrzędne także zostaną usunięte!',
+'Where is this file?' => 'Gdzie jest ten plik?',
+'You can find this file here:' => 'Możesz znaleźć ten plik tutaj:',
+'Associated files' => 'Powiązane pliki',
+'Select one or more tags to view the associated files.' => 'Wybierz jeden lub więcej tagów, aby wyświetlić powiązane z nim pliki.',
+
+// - JavaScript
+'Unable to get files list!' => 'Nie można uzyskać listy plików!',
+'Tag moved successfully!' => 'Tag zaostał pomyślnie przeniesiony!',
+'Tag not moved! Data base error!' => 'Tag nie został przeniesiony! Błąd bazy danych!',
+'Tag not moved! Ajax error!' => 'Tag nie został przeniesiony! Błąd Ajax!',
+'Rename' => 'Zmień nazwę',
+'New' => 'Nowy',
+'Delete' => 'Usuń',
+'Nothing possible' => 'Brak możliwości',
+'Editing of Root node not allowed!' => 'Edycja tagu nadrzędnego niedozwolona!',
+'Deleting of Root node not allowed!' => 'Usuwanie tagu nadrzędnego niedozwolone!',
+'Lenght must be between ' => 'Długość dozwolona pomiędzy: ',
+'Cancel' => 'Anuluj',
+'Rename done!' => 'Zmiana nazwy przebiegła pomyślnie!',
+'Unable to rename! Data base error!' => 'Zmiana nazwy niemożliwa! Błąd bazy danych!',
+'Unable to rename! Ajax error!' => 'Zmiana nazwy niemożliwa! Błąd Ajax',
+'Tag created successfully!' => 'Tag został utworzony pomyślnie!',
+'Unable to create tag! Data base error!' => 'Utworzenie tagu niemożliwe! Błąd bazy danych!',
+'Unable to create tag! Ajax error!' => 'Utworzenie tagu niemożliwe! Błąd Ajax!',
+'Operation canceled: No deletion occurred!' => 'Operacja anulowana: Nie nastąpiło usunięcie!',
+'Invalid tag number! Nothing done!' => 'Niewłaściwy numer taga! Nic nie zostało zmienione.',
+'Tag removed successfully!' => 'Tag został pomyślnie usunięty!',
+'Tag not removed! Data base error!' => 'Tag nie został usunięty! Błąd bazy danych!',
+'Tags not removed! Ajax error!' => 'Tag nie został usunięty! Błąd Ajax!',
+'Confirm' => 'Potwierdź',
+
+// Extended info popup
+// - JavaScript
+'Tag selected files' => 'Otaguj wybrane pliki',
+'Unable to get user\'s priviledge.' => 'Nie można uzyskać uprawnień użytkownika.',
+'Informations' => 'Informacje',
+'Enter tags here' => 'Tu wpisz tagi',
+'File(s) where the tags will be applied' => 'Pliki, dla których tagi zostaną zastosowane.',
+'Tags common to all files' => 'Tagi wspólne dla wszystkich plików:',
+'Tags for the selected file' => 'Tagi dla wybranego pliku:',
+'The tag "' => 'Tag "',
+'" doesn\'t exist; would you like to create a new one?' => '" nie istnieje! Czy chcesz utworzyć nowy tag o tej nazwie?',
+'Unable to create the tag! Ajax error.' => 'Nie można utworzyć tagu! Błąd Ajax!',
+'Unable to add the tag! Ajax error.' => 'Nie można dodać tagu! Błąd Ajax!',
+'Unable to remove the tag! Data base error.' => 'Nie można usunąć tagu! Błąd bazy danych!',
+'Unable to remove the tag! Ajax error.' => 'Nie można usunąć tagu! Błąd Ajax!',
+'Unable to get the tags! Ajax error.' => 'Nie można pobrać tagów! Błąd Ajax!',
+'Unable to retrieve informations on this file! Ajax error!' => 'Nie można pobrać informacji o tym pliku! Błąd Ajax!',
+'Unable to get actual tags for this document! Ajax error!' => 'Nie można pobrać aktualnych tagów dla tego pliku! Błąd Ajax!',
+'All selected files' => 'Wszystkie zaznaczone pliki',
+'Informations on' => 'Informaciej o',
+'Informations' => 'Informacje',
+
+// getFileInfo.php
+'Multiple files selected' => 'Zaznaczono wiele plików',
+'File name' => 'Nazwa pliku: ',
+'Size' => 'Rozmiar: ',
+'When added' => 'Data dodania: ',
+'Encrypted? ' => 'Zaszyfrowane? ',
+'Yes' => 'Tak',
+'No' => 'Nie',
+'Unencrypted size' => 'Niezaszyfrowany rozmiar'
+);
